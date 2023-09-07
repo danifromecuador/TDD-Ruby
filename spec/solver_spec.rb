49d42205ee6efacf8beb4d979ea33e91effca0e3
PabloBona <pablobonasera@gmail.com>
2023-09-07 15:26:02 -0300

    Fix Lint
    Fix Lints

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,24 +1,23 @@
 # import solver file
 
 # test if class Solver exists
-  # test if class Solver has a method factorial
-    # test special case when the argument is 0 this method sould return 1
-    # test if argument is negative this method should return some warning
-    # test if argument is not an integer this method should return some warning
-    # test if argument is 3 this method should return 6 (3 * 2 * 1)
-    # test if argument is 9 this method should return 362880
-  # test if class Solver has a method reverse
-    # test if argument is a string, other way return some warning
-    # test if argument is "hello" this method should return "olleh"    
-  # test if class Solver has a method fizzbuzz
-    # test if argument is not an integer this method should return some warning
-    # test if the returned value is a string
-    # test if argument is divisible by 3 this method should return "fizz"
-      # test if argument is 9 this method should return "fizz"
-    # test if argument is divisible by 5 this method should return "buzz"
-      # test if argument is 50 this method should return "buzz"
-    # test if argument is divisible by 3 and 5 this method shoudl return "fizzbuzz"
-      # test if argument is 15 this method should return "fizzbuzz"
-    # test if argument is not divisible by 3 or 5 this method should return the argument
-      # test if argument is 7 this method should return 7
-   
+# test if class Solver has a method factorial
+# test special case when the argument is 0 this method sould return 1
+# test if argument is negative this method should return some warning
+# test if argument is not an integer this method should return some warning
+# test if argument is 3 this method should return 6 (3 * 2 * 1)
+# test if argument is 9 this method should return 362880
+# test if class Solver has a method reverse
+# test if argument is a string, other way return some warning
+# test if argument is "hello" this method should return "olleh"
+# test if class Solver has a method fizzbuzz
+# test if argument is not an integer this method should return some warning
+# test if the returned value is a string
+# test if argument is divisible by 3 this method should return "fizz"
+# test if argument is 9 this method should return "fizz"
+# test if argument is divisible by 5 this method should return "buzz"
+# test if argument is 50 this method should return "buzz"
+# test if argument is divisible by 3 and 5 this method shoudl return "fizzbuzz"
+# test if argument is 15 this method should return "fizzbuzz"
+# test if argument is not divisible by 3 or 5 this method should return the argument
+# test if argument is 7 this method should return 7
