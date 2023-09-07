c0393c70dde5a1147ab54d4b1bbc7c3b39c2c3a7
dani <danifromecuador@gmail.com>
2023-09-07 16:11:54 -0500

    Create tests for factorial and reverse methods

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,23 +1,37 @@
 # import solver file
+require_relative '../solver'
 
-# test if class Solver exists
-# test if class Solver has a method factorial
-# test special case when the argument is 0 this method sould return 1
-# test if argument is negative this method should return some warning
-# test if argument is not an integer this method should return some warning
-# test if argument is 3 this method should return 6 (3 * 2 * 1)
-# test if argument is 9 this method should return 362880
-# test if class Solver has a method reverse
-# test if argument is a string, other way return some warning
-# test if argument is "hello" this method should return "olleh"
-# test if class Solver has a method fizzbuzz
-# test if argument is not an integer this method should return some warning
-# test if the returned value is a string
-# test if argument is divisible by 3 this method should return "fizz"
-# test if argument is 9 this method should return "fizz"
-# test if argument is divisible by 5 this method should return "buzz"
-# test if argument is 50 this method should return "buzz"
-# test if argument is divisible by 3 and 5 this method shoudl return "fizzbuzz"
-# test if argument is 15 this method should return "fizzbuzz"
-# test if argument is not divisible by 3 or 5 this method should return the argument
-# test if argument is 7 this method should return 7
+describe Solver do
+  describe "Solver" do
+    it "is a class" do # test if class Solver exists
+      expect(Solver.new).to be_a(Solver)
+    end
+  end
+
+  describe "#factorial" do
+    it "is a method" do # test if class Solver has a method factorial
+      expect(Solver.new).to respond_to(:factorial)
+    end
+    it "returns 1 when argument is 0" do # test special case when the argument is 0 this method sould return 1
+      expect(Solver.new.factorial(0)).to eq(1)
+    end
+    it "returns 6 when argument is 3" do # test if argument is 3 this method should return 6 (3 * 2 * 1)
+      expect(Solver.new.factorial(3)).to eq(6)
+    end
+    it "returns 362880 when argument is 9" do # test if argument is 9 this method should return 362880
+      expect(Solver.new.factorial(9)).to eq(362880)
+    end
+  end
+
+  describe "#reverse" do
+    it "is a method" do # test if class Solver has a method reverse
+      expect(Solver.new).to respond_to(:reverse)
+    end
+    it "returns a string" do # test if the returned value is a string
+      expect(Solver.new.reverse("hello")).to be_a(String)
+    end
+    it "returns olleh when argument is hello" do # test if argument is "hello" this method should return "olleh"
+      expect(Solver.new.reverse("hello")).to eq("olleh")
+    end
+  end
+end
